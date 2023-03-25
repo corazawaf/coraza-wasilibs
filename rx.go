8f8cadd87c661b0ec164d17ccdff94a19d728044
Anuraag Agrawal <anuraaga@gmail.com>
2023-03-25 09:08:50 +0900

    Update to latest coraza and enable multiline mode, latin1 for rx (#4)

--- rx.go
+++ rx.go
@@ -4,9 +4,14 @@
 package wasilibs
 
 import (
+	"fmt"
+	"strconv"
+	"unicode/utf8"
+
 	"github.com/corazawaf/coraza/v3/operators"
 	"github.com/corazawaf/coraza/v3/rules"
 	"github.com/wasilibs/go-re2"
+	"github.com/wasilibs/go-re2/experimental"
 )
 
 type rx struct {
@@ -16,37 +21,77 @@ type rx struct {
 var _ rules.Operator = (*rx)(nil)
 
 func newRX(options rules.OperatorOptions) (rules.Operator, error) {
-	o := &rx{}
-	data := options.Arguments
+	// (?sm) enables multiline mode which makes 942522-7 work, see
+	// - https://stackoverflow.com/a/27680233
+	// - https://groups.google.com/g/golang-nuts/c/jiVdamGFU9E
+	data := fmt.Sprintf("(?sm)%s", options.Arguments)
 
-	re, err := re2.Compile(data)
+	var re *re2.Regexp
+	var err error
+
+	if matchesArbitraryBytes(data) {
+		re, err = experimental.CompileLatin1(data)
+	} else {
+		re, err = re2.Compile(data)
+	}
 	if err != nil {
 		return nil, err
 	}
-
-	o.re = re
-	return o, err
+	return &rx{re: re}, nil
 }
 
 func (o *rx) Evaluate(tx rules.TransactionState, value string) bool {
-	match := o.re.FindStringSubmatch(value)
-	if len(match) == 0 {
-		return false
-	}
-
 	if tx.Capturing() {
+		match := o.re.FindStringSubmatch(value)
+		if len(match) == 0 {
+			return false
+		}
 		for i, c := range match {
 			if i == 9 {
 				return true
 			}
 			tx.CaptureField(i, c)
 		}
+		return true
+	} else {
+		return o.re.MatchString(value)
 	}
-
-	return true
 }
 
 // RegisterRX registers the rx operator using a WASI implementation instead of Go.
 func RegisterRX() {
 	operators.Register("rx", newRX)
+}
+
+// matchesArbitraryBytes checks for control sequences for byte matches in the expression.
+// If the sequences are not valid utf8, it returns true.
+func matchesArbitraryBytes(expr string) bool {
+	decoded := make([]byte, 0, len(expr))
+	for i := 0; i < len(expr); i++ {
+		c := expr[i]
+		if c != '\\' {
+			decoded = append(decoded, c)
+			continue
+		}
+		if i+3 >= len(expr) {
+			decoded = append(decoded, expr[i:]...)
+			break
+		}
+		if expr[i+1] != 'x' {
+			decoded = append(decoded, expr[i])
+			continue
+		}
+
+		v, mb, _, err := strconv.UnquoteChar(expr[i:], 0)
+		if err != nil || mb {
+			// Wasn't a byte escape sequence, shouldn't happen in practice.
+			decoded = append(decoded, expr[i])
+			continue
+		}
+
+		decoded = append(decoded, byte(v))
+		i += 3
+	}
+
+	return !utf8.Valid(decoded)
 }
--- waf/go.mod
+++ waf/go.mod
@@ -4,8 +4,9 @@ go 1.19
 
 require (
 	github.com/bmatcuk/doublestar/v4 v4.4.0
-	github.com/corazawaf/coraza/v3 v3.0.0-20230125200438-bb55a14650d7
+	github.com/corazawaf/coraza-coreruleset v0.0.0-20230306071035-6b1388c13a62
 	github.com/corazawaf/coraza-wasilibs v0.0.0-00010101000000-000000000000
+	github.com/corazawaf/coraza/v3 v3.0.0-rc.1.0.20230323163300-b8e5c6dbdd05
 	github.com/coreruleset/go-ftw v0.4.5
 	github.com/rs/zerolog v1.28.0
 )
@@ -31,17 +32,17 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/petar-dambovaliev/aho-corasick v0.0.0-20211021192214-5ab2d9280aa9 // indirect
-	github.com/tetratelabs/wazero v1.0.0-pre.7 // indirect
+	github.com/tetratelabs/wazero v1.0.0-rc.2 // indirect
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/wasilibs/go-aho-corasick v0.2.0 // indirect
 	github.com/wasilibs/go-libinjection v0.1.0 // indirect
-	github.com/wasilibs/go-re2 v0.1.0 // indirect
+	github.com/wasilibs/go-re2 v0.2.1 // indirect
 	github.com/yargevad/filepathx v1.0.0 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
-	golang.org/x/net v0.5.0 // indirect
-	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
